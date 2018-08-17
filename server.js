53e8b1e6d7c517ed43982360619fbd9fe92f032e
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-08-17 14:44:40 -0400

    adding directorys

--- server.js
+++ server.js
@@ -3,3 +3,23 @@
     Mongo-Scrape, 2018
     UofT SCS Coding Bootcamp
 */
+
+//Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const request = require('request');
+var exphbs  = require('express-handlebars');
+
+//Scraping tool
+const cheerio = require('cheerio');
+
+var PORT = 3000;
+
+//Init Express
+const app = express();
+
+//body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+//express.static to serve public folder as static directory
+app.use(express.static("public"));

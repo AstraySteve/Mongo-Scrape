3a17b8056b29e50fc55785cb5493569158a12e0c
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-08-17 18:37:57 -0400

    scrape added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,9 +4,30 @@ const request = require('request');
 //Scraping tool
 const cheerio = require('cheerio');
 
-module.exports = function(app){
+module.exports =(app)=>{
     //home page
-    app.get("/", function(req, res){
+    app.get("/",(req, res)=>{
         res.render("index");
+    });
+
+    //get/ scrape articles
+    app.get("/scrape", (req, res)=>{
+        request("https://myanimelist.net/news",(error,res,html)=>{
+            var $ = cheerio.load(html);
+            var results = []
+            $("div.news-unit-right").each((i,element)=>{
+                var title = $(element).children($('p.title')).children().text();
+                var link = $(element).children($('p.title')).children().attr("href");
+                var summary = $(element).children($('div.text')).text();
+
+                results.push({
+                    title: title,
+                    link: link,
+                    summary: summary.trim(),
+                });
+            });
+            console.log(results);
+        });
+        res.send("scrape complete");
     });
 };
